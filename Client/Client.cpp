36691d9660fe137a5974f14f82d3cc8139a89063
Knighthood2001 <2109695291@qq.com>
2026-01-16 12:58:13 +0800

    基本解决数据粘连和丢包的问题

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -3,6 +3,17 @@
 #include <cstring>  // 用于 strlen 函数
 
 #pragma comment(lib, "ws2_32.lib")
+#pragma pack(push, 1) // 将这个结构体按照一字节对齐
+struct PacketHeader {
+    int magic; // 4字节包头
+    int cmd; // 4字节命令
+    int body_len; // 数据长度
+};
+#pragma pack(pop)
+struct Packet {
+    PacketHeader header; // 包头
+    char body[]; // 包数据
+};
 int main()
 {
     // 初始化网络环境
@@ -21,7 +32,7 @@ int main()
     SOCKADDR_IN server_addr;
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = ntohs(9999);
-    server_addr.sin_addr.S_un.S_addr = inet_addr("192.168.212.80");
+    server_addr.sin_addr.S_un.S_addr = inet_addr("192.168.226.80");
 
     // 连接服务器
     if (connect(client_socket, (sockaddr*)&server_addr, sizeof(SOCKADDR_IN)) == SOCKET_ERROR) {
@@ -29,27 +40,36 @@ int main()
         return 0;
     }
     std::cout << "连接服务器成功\n" << std::endl;
+
+    //////////////////////////////////////////////////////////////////////////////////////
     char buffer[1024] = "hello world!";
     char receive_buffer[1024] = { 0 };  // 初始化数组，避免随机值;
-
+    int count = 0;
     while (true) {
-        int send_len = send(client_socket, buffer, strlen(buffer) + 1, 0);  // +1 携带字符串结束符 '\0'
-        if (send_len == SOCKET_ERROR) {
+        Packet* packet = (Packet*)malloc(sizeof(PacketHeader) + 10);
+        // 定义包头信息
+        packet->header.magic = 0x55AA77CC;
+        packet->header.cmd = 2000;
+        packet->header.body_len = 10;
+        // 将数据拷贝到要发送的packet中
+        memcpy(packet->body, buffer, 10);
+        int send_len = send(client_socket, (char*) & packet->header.magic, sizeof(PacketHeader)+packet->header.body_len, 0);  // +1 携带字符串结束符 '\0'
+        if (send_len == SOCKET_ERROR) { 
             std::cout << "发送数据失败" << std::endl;
             return 0;
         }
         std::cout << "client send data: " << buffer << std::endl;
         std::cout << "客户端发送数据成功，发送长度：" << send_len << std::endl;
-
-        // 等待接收数据
-        int receive_len = recv(client_socket, receive_buffer, sizeof(receive_buffer) - 1, 0);  // 预留1个位置给 '\0'
-        if (receive_len == SOCKET_ERROR) {
-            std::cout << "接收数据失败，错误码：" << WSAGetLastError() << std::endl;
-        }
-        else if (receive_len > 0) {
-            receive_buffer[receive_len] = '\0';  // 添加字符串结束符，避免乱码
-            std::cout << "client receive data: " << receive_buffer << std::endl;
-        }
+        free(packet);
+        //// 等待接收数据
+        //int receive_len = recv(client_socket, receive_buffer, sizeof(receive_buffer) - 1, 0);  // 预留1个位置给 '\0'
+        //if (receive_len == SOCKET_ERROR) {
+        //    std::cout << "接收数据失败，错误码：" << WSAGetLastError() << std::endl;
+        //}
+        //else if (receive_len > 0) {
+        //    receive_buffer[receive_len] = '\0';  // 添加字符串结束符，避免乱码
+        //    std::cout << "client receive data: " << receive_buffer << std::endl;
+        //}
     }
     // 关闭客户端socket，避免资源泄漏
     closesocket(client_socket);
--- RemoteControl/RemoteControl.cpp
+++ RemoteControl/RemoteControl.cpp
@@ -3,6 +3,19 @@
 #include <cstring>  // 用于 strlen 函数
 
 #pragma comment(lib, "ws2_32.lib")
+#define RECV_BUFFER_SIZE 1024*1024*1
+#pragma pack(push, 1) // 将这个结构体按照一字节对齐
+struct PacketHeader {
+    int magic; // 4字节包头
+    int cmd; // 4字节命令
+    int body_len; // 数据长度
+};
+#pragma pack(pop)
+struct Packet {
+    PacketHeader header; // 包头
+    char body[]; // 包数据
+};
+Packet* ParsePacket(char* buffer, int len);
 int main()
 {
     // 初始化网络环境
@@ -39,23 +52,30 @@ int main()
     std::cout << "等待客户端连接\n" << std::endl;
     SOCKET client_socket = accept(server_socket, (sockaddr*)&client_addr, &client_addr_len);
     // 等待客户端发送数据
-    char buffer[1024] = { 0 };  // 初始化数组，避免随机值;
+    char* buffer = (char*)malloc(RECV_BUFFER_SIZE);
+    // 记录缓冲区当前数据的长度
+    static int index = 0;
     while (true) {
-        int recv_len = recv(client_socket, buffer, sizeof(buffer) - 1, 0);
+        //int recv_len = recv(client_socket, buffer, sizeof(buffer) - 1, 0); // 这样存在数据丢失的问题，需要有记录缓存区数据长度
+        int recv_len = recv(client_socket, buffer+index, RECV_BUFFER_SIZE-index, 0); //缓冲区总大小为RECV_BUFFER_SIZE，已经使用了index个字节（前index个位置已存放上一次接收的有效数据），剩余可用空间为RECV_BUFFER_SIZE - index
+        index += recv_len;
         if (recv_len == SOCKET_ERROR) {
             std::cout << "客户端接收数据失败" << std::endl;
         }
         else if (recv_len > 0) {
-            buffer[recv_len] = '\0';  // 添加字符串结束符，避免乱码
-            std::cout << "server recv data: " << buffer << std::endl;
-
-            // 发送数据给客户端
-            int send_len = send(client_socket, buffer, strlen(buffer) + 1, 0);
-            if (send_len == SOCKET_ERROR) {
-                std::cout << "服务端回发数据失败" << std::endl;
-            }
-            std::cout << "send recv data: " << buffer << std::endl;
+            Packet* packet = ParsePacket(buffer, index);
+            index = index - packet->header.body_len + sizeof(PacketHeader);
+            memmove(buffer, buffer+packet->header.body_len+sizeof(PacketHeader), index);//把已经读取的数据删了
+            std::cout << "server recv data: " << packet->body << std::endl;
+            free(packet);
+            //// 发送数据给客户端
+            //int send_len = send(client_socket, buffer, strlen(buffer) + 1, 0);
+            //if (send_len == SOCKET_ERROR) {
+            //    std::cout << "服务端回发数据失败" << std::endl;
+            //}
+            //std::cout << "send recv data: " << buffer << std::endl;
         }
+        Sleep(200);
     }
     // 关闭Socket，避免资源泄漏（先关客户端连接Socket，再关监听Socket）
     closesocket(client_socket);
@@ -63,4 +83,33 @@ int main()
     // 清除网络环境
     WSACleanup();
     return 0;
+}
+
+Packet* ParsePacket(char* buffer, int len){
+    // magic cmd body_len data
+    Packet pck;
+    Packet* ppck;
+    int index = 0;
+    // 找包头，4字节
+    for (; index < len; index++) {
+        if (*(int*)(buffer + index) == 0x55AA77CC) {
+            pck.header.magic = *(int*)(buffer + index);
+            index += 4;
+            break;
+        }
+    }
+    // 命令
+    pck.header.cmd = *(int*)(buffer + index); index += 4;
+    pck.header.body_len = *(int*)(buffer + index); index += 4;
+    // 获取数据
+    if (pck.header.body_len > 0) {
+        // 创建接收缓存区
+        ppck = (Packet*)malloc(sizeof(PacketHeader) + pck.header.body_len);
+        // 拷贝用户数据
+        memcpy(ppck->body, buffer + index, pck.header.body_len);
+        // 拷贝包头
+        memcpy(&ppck->header, &pck.header, sizeof(PacketHeader));
+        return ppck;
+    }
+
 }
