7172347431d394607ae36d7ab4235dfc44ea94b0
Knighthood2001 <2109695291@qq.com>
2026-01-23 10:16:50 +0800

    添加鼠标事件的结构体

--- Common/PacketUtils.h
+++ Common/PacketUtils.h
@@ -3,6 +3,7 @@
 
 #include <cstring>  // 用于 strlen 函数
 #include <cstdlib>  // 用于 malloc 和 free
+#include <windef.h>
 
 #pragma pack(push, 1) // 将这个结构体按照一字节对齐
 struct PacketHeader {
@@ -22,6 +23,24 @@ enum CMD {
     CMD_MOUSE = 2,
     CMD_KEYBOARD = 4,
     CMD_TEST = 10,
+};
+
+enum ENUM_MOUSE {
+    MOUSE_MOVE = 1,  // 移动
+    MOUSE_LDOWN =2,  //左键按下
+    MOUSE_LUP = 3,
+    MOUSE_RDOWN = 4,  //右键按下
+    MOUSE_RUP = 5,
+    MOUSE_MDOWN =6,
+    MOUSE_MUP = 7,
+    MOUSE_LDLINK = 8, // 鼠标左键双击
+    MOUSE_RDLINK = 9,  // 右键双击
+    MOUSE_MDLINK = 10,
+};
+
+struct Mouse {
+    int action; // 鼠标行为
+    POINT ptXY; // 鼠标的坐标
 };
 
 // 获取数据包长度
